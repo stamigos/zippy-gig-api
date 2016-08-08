a81613c6f19868891bc2b3039d6b8513a652d4f3
Oleksii Yukhymenko <al.yukhymenko@gmail.com>
2016-08-08 16:12:05 +0300

    edit "profile edit"

--- zippy_gig/auth/edit_prof.py
+++ zippy_gig/auth/edit_prof.py
@@ -3,34 +3,40 @@ from hashlib import sha1
 from flask import g
 from zippy_gig.base import BaseController, ApiException
 import phonenumbers
+from zippy_gig.constants import AccountType
 
 
 class EditProfileController(BaseController):
     def __init__(self, request):
         super(EditProfileController, self).__init__(request)
 
-    def _call(self):
-        try:
-            g.account.first_name = self.request.json['first_name']
-            g.account.last_name = self.request.json['last_name']
-            g.account.address = self.request.json['address']
-            g.account.phone = self.check_phone(self.request.json['phone'])
-            g.account.alt_phone = self.check_phone(self.request.json['alt_phone'])
-            g.account.pay_pal = self.request.json['pay_pal']
-            g.account.type = self.request.json['type']
-            g.account.save()
-        except KeyError:
-            raise ApiException("Error in JSON")
-        else:
-            return g.account.get_profile()
+    def _call(self):       
+        g.account.first_name = self._verify_field('first_name')
+        g.account.last_name = self._verify_field('last_name')
+        g.account.address = self._verify_field('address')
+        g.account.phone = self.check_phone(self._verify_field('phone'))
+        g.account.alt_phone = self.check_phone(self._verify_field('alt_phone'))
+        g.account.pay_pal = self._verify_field('pay_pal')
+        g.account.type = self.check_type(self._verify_field('type'))
+        g.account.save()
+    
+        return g.account.get_profile()
 
     def check_phone(self, phone):
-        if phonenumbers.is_possible_number(phonenumbers.parse(phone, None)):
-            return phone
-        raise ApiException("Error in phone")
+        try:
+            parsed_phone_number = phonenumbers.parse(phone)
+        except ServiceException, ex:
+            raise ApiException('Invalid phone number: %s %s' % (phone, ex.message))
+            
+        if not phonenumbers.is_valid_number(parsed_phone_number):
+            raise ApiException('Invalid phone number: %s' % (phone))
+        
+        return phone
+            
 
     def check_type(self, _type):
-        if _type in ['1', '2', '3']:
+        [str(val) for name, val in vars(AccountType).iteritems() if not name.startswith('_')]
+        if _type in [str(val) for name, val in vars(AccountType).iteritems() if not name.startswith('_')]:
             return _type
-        raise ApiException("Error in type")
+        raise ApiException("Invalid type: %s" % (_type))
     
