404b029ba0da7247abdf0acef0320df362f61cde
Oleksii Yukhymenko <al.yukhymenko@gmail.com>
2016-08-10 14:35:37 +0300

    change views

--- zippy_gig/gig/__init__.py
+++ zippy_gig/gig/__init__.py
@@ -6,16 +6,20 @@ from zippy_gig.decorators import jsonify_result, validate_json
 
 gig = Blueprint('gig', __name__, url_prefix='/api/v1/gig')
 
-@gig.route("/", methods=['POST', 'GET'])
+@gig.route("/", methods=['POST'])
 @validate_json
 @jsonify_result
 def create_new_work():
-    if request.method == 'POST':
-        return CreateNewWorkController(request)()
-    else:
-        return GetAllWorksController(request)()    
-
+    return CreateNewWorkController(request)()
+    
 @gig.route("/<int:id>/")
 @jsonify_result
 def get_work(id):
     return GetWorkController(request)(id)
+
+@gig.route("/", methods=['GET'])
+@validate_json
+@jsonify_result
+def get_all_works():
+    return GetAllWorksController(request)()    
+
