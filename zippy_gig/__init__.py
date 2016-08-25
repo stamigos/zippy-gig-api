2b0d0da8e6d4c249246413644752de352752ffa9
Oleksii Yukhymenko <al.yukhymenko@gmail.com>
2016-08-26 01:24:03 +0300

    add JSONEncoder for DecimalField

--- zippy_gig/__init__.py
+++ zippy_gig/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_httpauth import HTTPTokenAuth
-
+from zippy_gig.JSONEncoder import JSONEncoder
 from httpauth import HTTPBasicAuth
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.config.from_object('config')
 app.debug = True
+app.json_encoder = JSONEncoder
 # CORS(app)
 # cors = CORS(app, resources={r"/": {"origins": "*"}})
 # app.config['CORS_HEADERS'] = 'Content-Type'
